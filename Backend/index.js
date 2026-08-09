df831a1d94551f21163849283458023d216f50ed
Diana <diana.gutierrez.el0q@est.uraccan.edu.ni>
2026-08-09 12:23:42 -0600

    cambios nombre archivos

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,9 @@ const { PrismaClient } = require("@prisma/client");
 // Importar rutas
 const authRoutes = require("./routes/authRoutes");
 const usuariosRoutes = require("./routes/usuariosRoutes");
-const perfilEmpresaRoutes = require("./routes/perfilEmpresaController");
-const postulacionesRoutes = require("./routes/postulacionesController");
-const vacantesRoutes = require("./routes/vacantesController");
+const perfilEmpresaRoutes = require("./routes/perfilEmpresaRoutes");
+const postulacionesRoutes = require("./routes/postulacionesRoutes");
+const vacantesRoutes = require("./routes/vacantesRoutes");
 const serviciosRoutes = require("./routes/serviciosRoutes");
 const seguirEmpresaRoutes = require("./routes/seguirEmpresaRoutes");
 
